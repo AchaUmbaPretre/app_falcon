b4bcf75f46657ad3d6f7c61fde00893f189d542c
AchaUmbaPretre <achandambi@gmail.com>
2024-05-14 13:43:22 +0100

    commit

--- src/pages/client/Client.jsx
+++ src/pages/client/Client.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './client.scss'
 import { Breadcrumb } from 'antd'
+import { PlusCircleOutlined } from '@ant-design/icons';
 
 const Client = () => {
 
@@ -10,6 +11,17 @@ const Client = () => {
       <div className="client">
         <div className="client_wrapper">
           <div className="client_wrapper_top">
+            <div className="client_text_row">
+              <div className="client_text_left">
+                <h2 className="client_h2">Client</h2>
+                <span className="client_span">Liste des clients</span>
+              </div>
+              <div className="client_text_right">
+                <button><PlusCircleOutlined /></button>
+              </div>
+            </div>
+          </div>
+          <div className="client_wrapper_center">
             <Breadcrumb
                 separator=">"
                 items={[
@@ -22,8 +34,10 @@ const Client = () => {
                   }
                 ]}
             />
+            <div className="client_wrapper_center_bottom">
+            aaaaaa
           </div>
-          <div className="client_wrapper_bottom"></div>
+          </div>
         </div>
       </div>
     </>
