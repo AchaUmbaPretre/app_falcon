8848eb9c8bc4485cffccc3a5e330e4840ef3f4a5
AchaUmbaPretre <achandambi@gmail.com>
2024-05-17 11:59:28 +0100

    commit

--- src/pages/personnel/Personnel.jsx
+++ src/pages/personnel/Personnel.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Breadcrumb, Table } from 'antd'
 import { PlusCircleOutlined, SisternodeOutlined,FilePdfOutlined,FileExcelOutlined,PrinterOutlined, SearchOutlined } from '@ant-design/icons';
+import config from '../../config';
+import axios from 'axios';
 
 const Personnel = () => {
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
   const [searchValue, setSearchValue] = useState('');
   const [open, setOpen] = useState(false);
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${DOMAIN}/users`);
+        setData(data);
+        setLoading(false)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [DOMAIN]);
 
 
   const columns = [
@@ -85,7 +103,7 @@ const Personnel = () => {
                   </div>
                 </div>
 
-                <Table dataSource={data} columns={columns} />
+                <Table dataSource={data} columns={columns} loading={loading} />
             </div>
           </div>
         </div>
