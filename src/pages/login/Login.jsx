0c99504059df2b5846a599310cd596d2b3db503f
AchaUmbaPretre <achandambi@gmail.com>
2024-05-21 10:33:27 +0100

    commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import './login.scss'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  
+  const [input, setInput] = useState({});
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handChange = (e) =>{
+    setInput((prev)=>({...prev, [ e.target.name]:e.target.value  }));
+  }
+
+  const handClick = async (e) =>{
+    
+  }
   return (
-    <div>Login</div>
+    <>
+        <div className="login">
+          <div className="login-container">
+            <div className="login-left">
+              <h1 className="login-h1"></h1>
+              <p className="login-desc">
+              </p>
+              <Link to={'/register'}>
+                <span>Tu n'as pas de compte ?</span>
+              </Link>
+              <Link to={'/register'} className="login-btn">Register</Link>
+            </div>
+            <div className="login-right">
+              <h1>Login</h1>
+              <form >
+                <input type="text" placeholder='username' name='username' onChange={handChange} />
+                <input type="password" placeholder='password' name='password' onChange={handChange} />
+                <button onClick={handClick}>Envoyer</button>
+                { error && <span className="error">{error}</span> }
+              </form>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
 import './register.scss'
+import axios from 'axios'
 
 const Register = () => {
-  
+
+  const [inputs, setInputs] = useState({});
+
+  const handChange = (e) =>{
+    setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
+  }
+  const [error, setError] = useState(false);
+
+  const handSubmit = async (e) => {
+  }
+
   return (
-    <div>Register</div>
+    <>
+        <div className="register">
+          <div className="register-container">
+            <div className="register-container">
+              <div className="register-left">
+                <h1 className="register-h1"></h1>
+                <p className="register-desc">
+                </p>
+                <Link to={'/login'}>
+                  <span>Avez-vous un compte ?</span>
+                </Link>
+                <Link href="" className="register-btn">Register</Link>
+              </div>
+              <div className="register-right">
+                <h1>Register</h1>
+                <form >
+                  <input type="text" placeholder='username' name='username' onChange={handChange} />
+                  <input type="email" placeholder='email' name='email' onChange={handChange}/>
+                  <input type="password" placeholder='password' name='password' onChange={handChange}/>
+                  <input type="text" placeholder='name' name='name' onChange={handChange}/>
+                  <button onClick={handSubmit} >Envoyer</button>
+                  { error && <span className="error">{error}</span>} 
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
