4c41e6d067962c44384cb63e6e89aee1b7ce3646
AchaUmbaPretre <achandambi@gmail.com>
2024-05-24 09:30:53 +0100

    commit

--- src/components/pageViews/PageViews.jsx
+++ src/components/pageViews/PageViews.jsx
@@ -3,8 +3,65 @@ import clientImg from './../../assets/clients.png'
 import operationImg from './../../assets/operation.png'
 import traceurImg from './../../assets/traceur.png'
 import vehiculeImg from './../../assets/vehicule.png'
+import { useEffect, useState } from 'react'
+import config from '../../config'
+import axios from 'axios'
 
 const PageViews = () => {
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+    const [client, setClient] = useState('');
+    const [operation, setOperation] = useState('');
+    const [traceur, setTraceur] = useState('');
+    const [vehicule, setVehicule] = useState('');
+    const [loading, setLoading] = useState('');
+
+    useEffect(() => {
+        const fetchClient = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/client/count`);
+            setClient(data[0].nbre_client);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchClient();
+    }, [DOMAIN]);
+
+    useEffect(() => {
+        const fetchOperation = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/operation/count`);
+            setOperation(data[0].nbre_operation);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchOperation();
+    }, [DOMAIN]);
+
+    useEffect(() => {
+        const fetchTraceur = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/traceur/count`);
+            setTraceur(data[0].nbre_traceur);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchTraceur();
+    }, [DOMAIN]);
+
+    useEffect(() => {
+        const fetchVehicule = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/vehicule/count`);
+            setVehicule(data[0].nbre_vehicule);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchVehicule();
+    }, [DOMAIN]);
 
   return (
     <>
@@ -16,8 +73,8 @@ const PageViews = () => {
                             <span className="page_rond"></span>
                             <h5 className='pageViews_h5'>CLIENT</h5>
                         </div>
-                        <h1 className="pageViews_h1">100</h1>
-                        <span className='pageViews_span'>No additional income</span>
+                        <h1 className="pageViews_h1">{client}</h1>
+                        <span className='pageViews_span'>Nombre de client</span>
                     </div>
                     <div className="pageViews_right">
                         <img src={clientImg} alt="" className="pageViews_right_img" />
@@ -29,7 +86,7 @@ const PageViews = () => {
                             <span className="page_rond"></span>
                             <h5 className='pageViews_h5'>OPERATION</h5>
                         </div>
-                        <h1 className="pageViews_h1">100</h1>
+                        <h1 className="pageViews_h1">{operation}</h1>
                         <span className='pageViews_span'>Nombre d'opération</span>
                     </div>
                     <div className="pageViews_right">
@@ -42,7 +99,7 @@ const PageViews = () => {
                             <span className="page_rond"></span>
                             <h5 className='pageViews_h5'>TRACEUR</h5>
                         </div>
-                        <h1 className="pageViews_h1">100</h1>
+                        <h1 className="pageViews_h1">{traceur}</h1>
                         <span className='pageViews_span'>Nombre de traceur</span>
                     </div>
                     <div className="pageViews_right">
@@ -55,7 +112,7 @@ const PageViews = () => {
                             <span className="page_rond"></span>
                             <h5 className='pageViews_h5'>VEHICULE</h5>
                         </div>
-                        <h1 className="pageViews_h1">100</h1>
+                        <h1 className="pageViews_h1">{vehicule}</h1>
                         <span className='pageViews_span'>Nombre de vehicule</span>
                     </div>
                     <div className="pageViews_right">
--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 /* baseURL: 'http://localhost:8080' */
-/*       REACT_APP_SERVER_DOMAIN : 'http://localhost:8080' */
-/* REACT_APP_SERVER_DOMAIN : 'https://apindoe.loginsmart-cd.com' */
+/*       REACT_APP_SERVER_DOMAIN : 'http://localhost:8070' */
+/* REACT_APP_SERVER_DOMAIN : 'https://apifalcon.loginsmart-cd.com' */
 
 export default {
     REACT_APP_SERVER_DOMAIN : 'http://localhost:8070'
