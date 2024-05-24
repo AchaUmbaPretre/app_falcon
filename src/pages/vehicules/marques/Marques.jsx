69a7dd9d90fef84f47c9d80dbd37dd4d3589c3f6
AchaUmbaPretre <achandambi@gmail.com>
2024-05-24 16:54:44 +0100

    commit

--- src/pages/vehicules/marques/Marques.jsx
+++ src/pages/vehicules/marques/Marques.jsx
@@ -59,11 +59,11 @@ const Marques = () => {
       width: "160px",
       render: (text, record) => (
         <Space size="middle">
-          <Popover  title="Voir les détails" trigger="hover">
+{/*           <Popover  title="Voir les détails" trigger="hover">
             <Link>
               <Button icon={<EyeOutlined />} style={{ color: 'green' }} />
             </Link>
-          </Popover>
+          </Popover> */}
           <Popover  title="Supprimer" trigger="hover">
             <Popconfirm
               title="Êtes-vous sûr de vouloir supprimer?"
