2a6da94ef1c383a3dae8dd25ed76ba3ed896d381
AchaUmbaPretre <achandambi@gmail.com>
2024-05-27 14:40:40 +0100

    commit

--- src/pages/recharge/form/Recharge_form.jsx
+++ src/pages/recharge/form/Recharge_form.jsx
@@ -1,10 +1,43 @@
 import './recharge_form.scss'
+import iconClient from './../../../assets/client_row.png'
 
 function Recharge_form() {
   return (
     <>
         <div className="recharge_form">
-            
+            <div className="recharge_form_wrapper">
+                <h2 className="recharge_client">Liste de clients</h2>
+                <div className="recharge_form_rows">
+                    <div className="recharge_form_row">
+                        <img src={iconClient} alt="" className="recharge_img" />
+                        <div className="recharge_form_bottom">
+                            <span className="recharge_span">Nom : Acha</span>
+                            <span className="recharge_span">Nombre de numero actif : Acha</span>
+                        </div>
+                    </div>
+                    <div className="recharge_form_row">
+                        <img src={iconClient} alt="" className="recharge_img" />
+                        <div className="recharge_form_bottom">
+                            <span className="recharge_span">Nom : Acha</span>
+                            <span className="recharge_span">Nbre de numero actif : Acha</span>
+                        </div>
+                    </div>
+                    <div className="recharge_form_row">
+                        <img src={iconClient} alt="" className="recharge_img" />
+                        <div className="recharge_form_bottom">
+                            <span className="recharge_span">Nom : Acha</span>
+                            <span className="recharge_span">Nbre de numero actif : Acha</span>
+                        </div>
+                    </div>
+                    <div className="recharge_form_row">
+                        <img src={iconClient} alt="" className="recharge_img" />
+                        <div className="recharge_form_bottom">
+                            <span className="recharge_span">Nom : Acha</span>
+                            <span className="recharge_span">Nbre de numero actif : Acha</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
   )
