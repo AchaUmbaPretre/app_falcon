6cb30c2c433c1deb7d624e42d89f7f0d5b04d910
AchaUmbaPretre <achandambi@gmail.com>
2024-05-28 15:13:22 +0100

    commit

--- src/pages/traceur/historique/TraceurHistorique.jsx
+++ src/pages/traceur/historique/TraceurHistorique.jsx
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import config from '../../../config';
 import { Image } from 'antd';
 import axios from 'axios';
 import moment from 'moment';
+import config from '../../../config';
 
 const TraceurHistorique = ({ id_traceur }) => {
   const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(`${DOMAIN}/traceur/historique?idTraceur=${id_traceur}`);
         setData(data);
-        setLoading(false)
+        setLoading(false);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
     fetchData();
   }, [DOMAIN, id_traceur]);
-
-  if (data?.length === 0) {
-    return <p>Ce traceur n'a pas d'historique</p>;
-  }
 
   if (loading) {
     return <p>Chargement...</p>;
@@ -33,61 +28,60 @@ const TraceurHistorique = ({ id_traceur }) => {
 
   return (
     <div className="operationDetail">
-      {data?.map(detail => (
+      {data.length === 0 &&  <p>Aucun historique disponible pour ce traceur.</p>}
+      {data.map(detail => (
         <div key={detail.id_operations} className="operationDetail_wrapper">
           <div className="operation_row">
             <span className="operation_span">Client : </span>
-            <span className="operation_desc">{detail.nom_client ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.nom_client ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Superviseur : </span>
-            <span className="operation_desc">{detail.superviseur ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.superviseur ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
-            <span className="operation_span">Ingenieur : </span>
-            <span className="operation_desc">{detail.technicien ?? 'N/A'} </span>
+            <span className="operation_span">Ingénieur : </span>
+            <span className="operation_desc">{detail.technicien ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Type d'opération : </span>
-            <span className="operation_desc">{detail.nom_type_operations ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.nom_type_operations ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Marque : </span>
-            <span className="operation_desc">{detail.nom_marque ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.nom_marque ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Matricule: </span>
-            <span className="operation_desc">{detail.matricule ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.matricule ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Traceur : </span>
-            <span className="operation_desc">{detail.numero_serie ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.numero_serie ?? 'N/A'}</span>
           </div>
-
           <div className="operation_row">
-            <span className="operation_span">Numero attribué: </span>
-            <span className="operation_desc">{detail.numero ?? 'N/A'} </span>
+            <span className="operation_span">Numéro attribué: </span>
+            <span className="operation_desc">{detail.numero ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Date d'opération : </span>
-            <span className="operation_desc">{detail.date_operation ? moment(detail.date_operation).format('DD-MM-YYYY') : 'N/A'} </span>
+            <span className="operation_desc">{detail.date_operation ? moment(detail.date_operation).format('DD-MM-YYYY') : 'N/A'}</span>
           </div>
-
           <div className="operation_row">
-            <span className="operation_span">Kilometre : </span>
-            <span className="operation_desc">{detail.kilometre ?? 'N/A'} </span>
+            <span className="operation_span">Kilomètre : </span>
+            <span className="operation_desc">{detail.kilometre ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Tension : </span>
-            <span className="operation_desc">{detail.tension ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.tension ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
-            <span className="operation_span">Probleme : </span>
-            <span className="operation_desc">{detail.probleme ?? 'N/A'} </span>
+            <span className="operation_span">Problème : </span>
+            <span className="operation_desc">{detail.probleme ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Observation : </span>
-            <span className="operation_desc">{detail.observation ?? 'N/A'} </span>
+            <span className="operation_desc">{detail.observation ?? 'N/A'}</span>
           </div>
           <div className="operation_row">
             <span className="operation_span">Photo plaque : </span>
