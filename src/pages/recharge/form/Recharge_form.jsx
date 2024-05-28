4268586e758b57f63866c8bfbb5f17db5e727049
AchaUmbaPretre <achandambi@gmail.com>
2024-05-28 15:50:45 +0100

    commit

--- src/pages/recharge/form/Recharge_form.jsx
+++ src/pages/recharge/form/Recharge_form.jsx
@@ -4,11 +4,13 @@ import config from '../../../config';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { SearchOutlined } from '@ant-design/icons';
 
 function Recharge_form() {
     const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
     const [client, setClient] = useState([]);
     const navigate = useNavigate();
+    const [searchValue, setSearchValue] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,13 +24,23 @@ function Recharge_form() {
         fetchData();
       }, [DOMAIN]);
 
+      const filteredData = client?.filter((item) => 
+        item.nom_client?.toLowerCase().includes(searchValue.toLowerCase())
+      )
+
   return (
     <>
         <div className="recharge_form">
             <div className="recharge_form_wrapper">
-                <h2 className="recharge_client">LISTE DES CLIENTS A RECHARGER</h2>
+                <div className="recharge_title_rows">
+                    <h2 className="recharge_client_title">LISTE DES CLIENTS A RECHARGER</h2>
+                    <div className="recharge_row_search">
+                        <SearchOutlined className='product-icon-plus' />
+                        <input type="search" name="" id=""  value={searchValue} onChange={(e) => setSearchValue(e.target.value)}  placeholder='Recherche...' className='product-search' />
+                    </div>
+                </div>
                 <div className="recharge_form_rows">
-                {client.map(dd => (
+                {filteredData.map(dd => (
                     <div className="recharge_form_row" onClick={()=> navigate(`/rechargeOne?id_client=${dd.id_client}`)}>
                         <img src={iconClient} alt="" className="recharge_img" />
                         <div className="recharge_form_bottom">
