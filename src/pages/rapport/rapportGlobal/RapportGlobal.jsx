2e28d6e55c5ff4abe137d2bde08edfbc63b6be2f
AchaUmbaPretre <achandambi@gmail.com>
2024-06-17 12:48:29 +0100

    commit

--- src/pages/rapport/rapportGlobal/RapportGlobal.jsx
+++ src/pages/rapport/rapportGlobal/RapportGlobal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './rapportGlobal.scss'
+import { Tabs } from 'antd'
 
 const RapportGlobal = () => {
   return (
@@ -7,10 +8,23 @@ const RapportGlobal = () => {
         <div className="rapportGlobal">
             <div className="rapportWrapper">
                 <div className="rapport_left">
-
+                    <h2 className="rapport_h2">Rapport de dépenses de Ndoé</h2>
+                    <div className="rapport_wrapper_pan">
+                        <Tabs>
+                            <Tabs.TabPane tab='Aujourdui' key={1}>
+                            </Tabs.TabPane>
+                            <Tabs.TabPane tab="Hier" key={2}>
+                            </Tabs.TabPane>
+                            <Tabs.TabPane tab='7 derniers jours' key={3}>
+                            </Tabs.TabPane>
+                            <Tabs.TabPane tab='30 derniers jours' key={4}>
+                            </Tabs.TabPane>
+                        </Tabs>
+                    </div>
                 </div>
                 <div className="rapport_right">
-                    
+                <h2 className="rapport_h2">Rapport de dépenses de Falcon</h2>
+
                 </div>
             </div>
         </div>
