e0bb8ac1945807de3a81e366765e788851ed02ad
AchaUmbaPretre <achandambi@gmail.com>
2024-06-20 11:57:47 +0100

    commit

--- src/components/informationGen/InformationGen.jsx
+++ src/components/informationGen/InformationGen.jsx
@@ -6,24 +6,34 @@ import RapportInfoHier from '../../pages/rapportInfo/RapportInfoHier';
 import RapportInfo7jours from '../../pages/rapportInfo/RapportInfo7jours';
 import RapportInfo30jours from '../../pages/rapportInfo/RapportInfo30jours';
 import RapportInfo1an from '../../pages/rapportInfo/RapportInfo1an';
+import RapportDepense from '../../pages/rapportDepense/RapportDepense';
+import RapportDepenseHier from '../../pages/rapportDepense/RapportDepenseHier';
+import RapportDepense7jours from '../../pages/rapportDepense/RapportDepense7jours';
+import RapportDepense30jours from '../../pages/rapportDepense/RapportDepense30jours';
+import RapportDepense1an from '../../pages/rapportDepense/RapportDepense1an';
 const InformationGen = () => {
   return (
     <>
         <div className='informationGeneral'>
         <Tabs>
             <Tabs.TabPane tab="Aujourd'hui" key={0}>
+                <RapportDepense/>
                 <RapportInfoJour/>
             </Tabs.TabPane>
             <Tabs.TabPane tab='Hier' key={1}>
+                <RapportDepenseHier/>
                 <RapportInfoHier/>
             </Tabs.TabPane>
             <Tabs.TabPane tab='7 derniers jours' key={2}>
+                <RapportDepense7jours/>
                 <RapportInfo7jours/>
             </Tabs.TabPane>
             <Tabs.TabPane tab='30 derniers jours' key={3}>
+                <RapportDepense30jours/>
                 <RapportInfo30jours/>
             </Tabs.TabPane>
             <Tabs.TabPane tab="1 an" key={4}>
+                <RapportDepense1an/>
                 <RapportInfo1an/>
             </Tabs.TabPane>
         </Tabs>
---
+++ src/pages/rapportDepense/RapportDepense30jours.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './rapportDepense.scss'
+import { VerticalAlignTopOutlined } from '@ant-design/icons';
+import CountUp from 'react-countup';
+import { useState } from 'react';
+import config from '../../config';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+
+const RapportDepense30jours = () => {
+    const [depenses, setDepenses] = useState([]);
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/depense/count30jours`);
+            setDepenses(data[0]?.total_depense);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAIN]);
+
+  return (
+    <>
+        <div className="rowTotals">
+            <div className="rowTotal-wrapper">
+                <div className="rowTotal">
+                    <div className="rowTotal-left" style={{background : 'rgba(255, 0, 0, 0.164)'}}>
+                        <VerticalAlignTopOutlined className='rowTotalIcon' style={{color: 'red'}}/>
+                    </div>
+                    <div className="rowTotal-right">
+                        <h2><CountUp end={depenses}/>$</h2>
+                        <span className="rowTotal-span">Montant total des dépenses</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default RapportDepense30jours
