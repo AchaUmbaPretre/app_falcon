5e01b7c050f16b5fbd81deb122a6140a61abd8ab
AchaUmbaPretre <achandambi@gmail.com>
2024-07-27 13:03:34 +0100

    commit

--- src/pages/personnel/form/PersonnelForm.jsx
+++ src/pages/personnel/form/PersonnelForm.jsx
@@ -11,6 +11,7 @@ const PersonnelForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
+  const [telephone, setTelephone] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -18,72 +19,100 @@ const PersonnelForm = () => {
 
   const handleClick = async (e) => {
     e.preventDefault();
-  
+
     try {
       setIsLoading(true);
-      const res = await axios.post(`${DOMAIN}/users/register`, { username, email, password, role });
+      const res = await axios.post(`${DOMAIN}/users/register`, { username, email, password, role, telephone });
       if (res.data.success) {
-        message.success("Personnel est enregistré avec succès");
-        navigate('/');
-        window.location.reload();
+        message.success("Personnel enregistré avec succès");
+        navigate('/personnel');
       } else {
         message.error(res.data.message);
       }
     } catch (error) {
-      toast.error('Erreur lors de l\'enregistrement.');
+      toast.error("Erreur lors de l'enregistrement.");
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-  }
-  
+  };
+
   return (
-    <>
-        <div className="clientForm">
-          <div className="product-container">
-            <div className="product-container-top">
-              <div className="product-left">
-                <h2 className="product-h2">Un nouveau personnel</h2>
-                <span>Créer un nouveau personnel</span>
-              </div>
-            </div>
-            <div className="product-wrapper">
-              <div className="product-container-bottom">
-                <div className="form-controle">
-                  <label htmlFor="">Nom <span style={{color:'red'}}>*</span></label>
-                  <input type="text" name='username' className="form-input" value={username} onChange={(e) => setUsername(e.target.value)} required/>
-                </div>
-                <div className="form-controle">
-                  <label htmlFor="">Role <span style={{color:'red'}}>*</span></label>
-                  <select name='role' className="form-input" value={role} onChange={(e) => setRole(e.target.value)}>
-                    <option value="">Sélectionner un rôle</option>
-                    {roles.map((role) => (
-                      <option key={role} value={role}>{role}</option>
-                    ))}
-                  </select>
-                </div>
-                <div className="form-controle">
-                  <label htmlFor="">Telephone <span style={{color:'red'}}>*</span></label>
-                  <input type="tel" name='telephone' className="form-input" required />
-                </div>
-                <div className="form-controle">
-                  <label htmlFor="">Email <span style={{color:'red'}}>*</span></label>
-                  <input type="email" name="email" className="form-input" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-              </div>
-              <div className="form-submit">
-                <button className="btn-submit" onClick={handleClick} disabled={isLoading}>Envoyer</button>
-                {isLoading && (
-                  <div className="loader-container loader-container-center">
-                    <Spin size="large" />
-                  </div>
-            )}
-              </div>
-            </div>
+    <div className="clientForm">
+      <div className="product-container">
+        <div className="product-container-top">
+          <div className="product-left">
+            <h2 className="product-h2">Un nouveau personnel</h2>
+            <span>Créer un nouveau personnel</span>
           </div>
         </div>
-    </>
-  )
-}
+        <div className="product-wrapper">
+          <div className="product-container-bottom">
+            <div className="form-controle">
+              <label>Nom <span style={{ color: 'red' }}>*</span></label>
+              <input
+                type="text"
+                name="username"
+                className="form-input"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-controle">
+              <label>Role <span style={{ color: 'red' }}>*</span></label>
+              <select
+                name="role"
+                className="form-input"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                required
+              >
+                <option value="">Sélectionner un rôle</option>
+                {roles.map((role) => (
+                  <option key={role} value={role}>{role}</option>
+                ))}
+              </select>
+            </div>
+            <div className="form-controle">
+              <label>Téléphone <span style={{ color: 'red' }}>*</span></label>
+              <input
+                type="tel"
+                name="telephone"
+                className="form-input"
+                onChange={(e) => setTelephone(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-controle">
+              <label>Email <span style={{ color: 'red' }}>*</span></label>
+              <input
+                type="email"
+                name="email"
+                className="form-input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="form-submit">
+            <button
+              className="btn-submit"
+              onClick={handleClick}
+              disabled={isLoading}
+            >
+              Envoyer
+            </button>
+            {isLoading && (
+              <div className="loader-container loader-container-center">
+                <Spin size="large" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PersonnelForm;
