61a07f24d54035e3df1436691d05069ab7a146f6
AchaUmbaPretre <achandambi@gmail.com>
2026-02-06 15:48:41 +0100

    commit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,40 +1,111 @@
-import React from 'react'
-import { MenuFoldOutlined, BellOutlined, MailOutlined } from '@ant-design/icons';
+import React, { useState } from 'react'
+import {
+  MenuFoldOutlined,
+  BellOutlined,
+  MailOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+  UserOutlined,
+} from '@ant-design/icons'
+import { Dropdown, Menu } from 'antd'
 import './topbar.scss'
-import users from './../../assets/user.png'
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleSidebar } from '../../redux/userRedux';
+import userAvatar from './../../assets/user.png'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleSidebar } from '../../redux/userRedux'
+import { useNavigate } from 'react-router-dom'
+import config from '../../config'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Topbar = () => {
+  const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+  const dispatch = useDispatch()
   const user = useSelector((state) => state.user.currentUser.username);
-  const dispatch = useDispatch();
-  const handleClick = () => {
-    dispatch(toggleSidebar());
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const navigate = useNavigate();
+  
+  const handleMenuClick = ({ key }) => {
+    if (key === 'logout') {
+      Logout()
+    } else if (key === 'settings') {
+      console.log('Paramètres...')
+    } else if (key === 'profile') {
+      console.log('Profil utilisateur...')
+    }
+    setDropdownVisible(false)
+  }
+
+    const Logout = async () => {
+    try {
+      await axios.post(`${DOMAIN}/users/logout`);
+      localStorage.removeItem('persist:root');
+      toast.success('Déconnexion réussie !');
+      navigate('/login');
+      window.location.reload();
+    } catch (error) {
+      toast.error('Erreur lors de la déconnexion.');
+    }
   };
 
+  const menu = (
+    <Menu onClick={handleMenuClick} className="topbar-dropdown-menu">
+      <Menu.Item key="profile" icon={<UserOutlined />}>
+        Profil
+      </Menu.Item>
+      <Menu.Item key="settings" icon={<SettingOutlined />}>
+        Paramètres
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="logout" icon={<LogoutOutlined />} danger>
+        Déconnexion
+      </Menu.Item>
+    </Menu>
+  )
+
   return (
-    <>
-      <div className="topbar">
-        <div className="topbar-wrapper">
-          <div className="topbar_left">
-            <MenuFoldOutlined onClick={handleClick} />
+    <header className="topbar">
+      <div className="topbar__container">
+        {/* LEFT */}
+        <div className="topbar__left">
+          <button
+            className="topbar__menu-btn"
+            onClick={() => dispatch(toggleSidebar())}
+            aria-label="Toggle sidebar"
+          >
+            <MenuFoldOutlined />
+          </button>
+        </div>
+
+        <div className="topbar__right">
+          <div className="topbar__actions">
+            <button className="topbar__icon-btn">
+              <MailOutlined />
+              <span className="badge">3</span>
+            </button>
+
+            <button className="topbar__icon-btn">
+              <BellOutlined />
+              <span className="badge">5</span>
+            </button>
           </div>
-          <div className="topbar_right">
-            <div className="topbar_icons topbarOne">
-              <MailOutlined className='topbar_icon'/>
+
+          <Dropdown
+            overlay={menu}
+            trigger={['click']}
+            visible={dropdownVisible}
+            onVisibleChange={(flag) => setDropdownVisible(flag)}
+          >
+            <div className="topbar__user">
+              <img src={userAvatar} alt="User" />
+              <div className="topbar__user-info">
+                <span className="name">{user}</span>
+                <span className="role">Administrateur</span>
+              </div>
             </div>
-            <div className="topbar_icons topbarOne">
-              <BellOutlined className='topbar_icon' />
-            </div>
-            <div className="topbar_icons">
-              <img src={users} alt="" className='topbar_user'/>
-              <span className='topbar_username'>{user}</span>
-            </div>
-          </div>
+          </Dropdown>
         </div>
       </div>
-
-    </>
+    </header>
   )
 }
 
