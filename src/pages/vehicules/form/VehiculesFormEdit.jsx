69ec7292a52c5e67b62d2a96bc4fcc440130ed25
AchaUmbaPretre <achandambi@gmail.com>
2026-02-18 11:44:11 +0100

    commit

--- src/pages/vehicules/form/VehiculesFormEdit.jsx
+++ src/pages/vehicules/form/VehiculesFormEdit.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react'
+import config from '../../../config';
+import { message } from 'antd';
 
 const VehiculesFormEdit = ({id, onClose, onSave}) => {
-    console.log(id)
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = useCallback(async () => {
+        setLoading(true);
+
+        try {
+            const response = await axios.get(`${DOMAIN}/vehicule/one`, {params:{id_vehicule: id}})
+            setData(response.data)
+        } catch (error) {
+            console.error(error);
+            message.error('Failed to fetch data');
+        }
+    }, [id]);
+
+    useEffect(()=> {
+        fetchData()
+    }, [])
+
   return (
     <>
         aaaaaa
